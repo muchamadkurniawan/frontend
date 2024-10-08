8dd634513430e3985c00967b3e8f54e7af90805e
kurniawan86@gmail <muchamad.kurniawan@gmail.com>
2024-10-08 13:40:18 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<script src="https://cdn.tailwindcss.com"></script>
+<head>
+    <meta charset="UTF-8">
+    <title>Title Coba</title>
+</head>
+<body>
+<h1 class="text-4xl font-bold text-red-900">
+    hello World
+</h1>
+</body>
+</html>
---
+++ kosong.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List Role</title>
+<!--    <link href="./Views/output.css" rel="stylesheet">-->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-100 font-sans leading-normal tracking-normal">
+
+    <!-- Navbar -->
+    <?php include 'includes/navbar.php'; ?>
+
+    <!-- Main container -->
+    <div class="flex">
+        <!-- Sidebar -->
+        <?php include 'includes/sidebar.php'; ?>
+
+        <!-- Main Content -->
+        <div class="flex-1 p-8">
+            <!-- Your main content goes here -->
+            INI HALAMAN KOSONG TES KELAS P1
+        </div>
+    </div>
+
+</body>
+</html>
